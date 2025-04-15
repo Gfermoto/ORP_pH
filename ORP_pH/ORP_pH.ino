8aaa0cd416cfe21e1f59780d3988acdd79ca8489
Gfermoto <gfermoto@gmail.com>
2025-04-15 14:01:07 +0300

    Исправлена кодировка файла README.md

--- ORP_pH/ORP_pH.ino
+++ ORP_pH/ORP_pH.ino
@@ -3,6 +3,7 @@
 #include <PubSubClient.h>
 #include <ArduinoJson.h>
 #include <Preferences.h>
+#include <ArduinoOTA.h>
 
 // Конфигурация WiFi
 const char* AP_SSID = "WaterSensor";
@@ -11,6 +12,10 @@ const char* MQTT_SERVER = "mqtt.local";
 const int MQTT_PORT = 1883;
 const char* MQTT_USER = "";
 const char* MQTT_PASSWORD = "";
+
+// Конфигурация OTA
+const char* OTA_HOSTNAME = "pool-sensor";
+const char* OTA_PASSWORD = "12345678";
 
 // Конфигурация пинов
 const int phPin = 34;    // GPIO34 для pH датчика
@@ -298,14 +303,45 @@ void setup() {
   analogReadResolution(12);
   analogSetAttenuation(ADC_11db);
   
-  // Настройка Preferences
+  // Инициализация EEPROM
   preferences.begin("water_sensor", false);
-  
-  // Загрузка настроек
   loadSettings();
   
   // Настройка WiFi
   setupWiFi();
+  
+  // Настройка OTA
+  ArduinoOTA.setHostname(OTA_HOSTNAME);
+  ArduinoOTA.setPassword(OTA_PASSWORD);
+  
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else { // U_SPIFFS
+      type = "filesystem";
+    }
+    Serial.println("Start updating " + type);
+  });
+  
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+  
+  ArduinoOTA.begin();
   
   // Настройка MQTT
   mqttClient.setServer(settings.mqttServer, settings.mqttPort);
@@ -342,6 +378,7 @@ void setup() {
 }
 
 void loop() {
+  ArduinoOTA.handle();
   server.handleClient();
   
   // Чтение pH
